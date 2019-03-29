d17af9c51af27416f5603309e87588584387107e
Ivan <ivan1991king@gmail.com>
2019-03-29 23:14:44 +0300

    del

--- task5.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Куркин Иван'
-
-count = 1
-for x in range(32, 128):
-    print(f"{x}-{chr(x)}", end=" ")
-    if count % 10 == 0:
-        print()
-    count += 1
--- task9.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Куркин Иван'
-
-numList = list(input('Введите числа через пробел: ').split())
-sums = []
-for num in numList:
-    sums += [sum(map(int, list(num)))]
-print("Наибольшее по сумме цифр число:", numList[sums.index(max(sums))])
-print("Максимальная сумма цифр:", max(sums))
